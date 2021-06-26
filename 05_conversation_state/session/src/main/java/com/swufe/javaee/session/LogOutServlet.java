d0a0ec0954e450c81f7f0dfdc6bc42ae6931fed1
TrafalgarZhao <81305286+TrafalgarZhao@users.noreply.github.com>
2021-06-27 00:47:12 +0800

    Add files via upload

---
+++ 05_conversation_state/session/src/main/java/com/swufe/javaee/session/LogOutServlet.java
@@ -0,0 +1,20 @@
+package com.swufe.javaee.session;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "LogOutServlet", value = "/log-out")
+public class LogOutServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getSession().invalidate();
+        response.sendRedirect("index");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
